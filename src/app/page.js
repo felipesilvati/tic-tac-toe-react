884d64ee9860e88ae1db40ed02f8992e9eb2aeac
Felipe Silva <felipesilva2004@hotmail.com>
2024-01-05 19:40:05 -0300

    refactor(winner_calc): move to another file with basic tests

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react';
+import { calculateWinner } from './utils';
 
 // Square Component
 function Square({ value, onClick }) {
@@ -48,27 +49,6 @@ function Board() {
       </div>
     </div>
   );
-}
-
-// Helper function to calculate the winner
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 // Home Component
--- src/app/utils.test.js
+++ src/app/utils.test.js
@@ -1,7 +1,18 @@
+import { calculateWinner } from './utils';
+
 describe('utils', () => {
-  describe('basic test', () => {
-    it('should pass', () => {
-      expect(true).toBe(true)
-    })
+  describe('calculateWinner', () => {
+    it('should return null if there is no winner', () => {
+      const squares = Array(9).fill(null);
+      expect(calculateWinner(squares)).toBe(null);
+    });
+    it('should return X if X is the winner', () => {
+      const squares = ['X', 'X', 'X', 'O', 'O', null, null, null, null];
+      expect(calculateWinner(squares)).toBe('X');
+    });
+    it('should return O if O is the winner', () => {
+      const squares = ['X', 'X', null, 'O', 'O', 'O', null, null, null];
+      expect(calculateWinner(squares)).toBe('O');
+    });
   })
 })
