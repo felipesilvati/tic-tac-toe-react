cc0320a502dca3477140693a490a6fd0d5157a0e
Felipe Silva <felipesilva2004@hotmail.com>
2024-01-09 15:20:07 -0300

    feat(mobile): increase the size of square on mobile

--- src/app/components/square.js
+++ src/app/components/square.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Square = ({ value, onClick }) => {
   return (
     <div
-      className="h-8 w-8 xs:h-16 xs:w-16 sm:h-24 sm:w-24 border-2 border-gray-400 flex items-center justify-center text-sm sm:text-4xl"
+      className="h-14 w-14 xs:h-16 xs:w-16 sm:h-24 sm:w-24 border-2 border-gray-400 flex items-center justify-center text-md sm:text-4xl"
       onClick={onClick}
     >
       {value}
